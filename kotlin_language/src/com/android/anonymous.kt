42294cacdff7a58b989190b731fce8ded9353d9c
Tecode <283731869@qq.com>
2022-02-28 21:41:39 +0800

    kotlin代码学习，lambda函数与普通函数

--- kotlin_language/src/com/android/anonymous.kt
+++ kotlin_language/src/com/android/anonymous.kt
@@ -9,9 +9,20 @@ fun main() {
     }
     println(len002)
     println(anonymousFunction(2, 3, 4))
+    println(func002("12", "23"))
+    println(fun003())
+    println(func004())
 }
 
 // 匿名函数实现参数传参
 val anonymousFunction: (Int, Int, Int) -> String = { number001, number002, number003 ->
     "number001:$number001 - number002:$number002 - number003:$number003"
 }
+
+// 匿名函数自动推断，自动根据返回的数据确定返回类型
+val func002 = {value001: String, value002: String -> value001 == value002 }
+
+// 匿名函数 == lambda表达式，匿名函数属于lambda
+val fun003 = { true }
+
+val func004 = { 3.1415 }
--- kotlin_language/src/com/android/function.kt
+++ kotlin_language/src/com/android/function.kt
@@ -1,6 +1,32 @@
 package com.android
 
 fun main() {
+    println(functionSum(functionString, 2, 54))
+//    lambda属于函数类型的对象::,需要把methodResponseResult普通函数变成函数类型的对象（函数引用）
+    login("haoxuan", "12345", ::methodResponseResult)
+    login("haoxuan", "1235", lambdaFunc)
 }
 
-typealias Func = () -> String;
+typealias FuncType = (Int, Int) -> String
+
+// 函数作为参数传递给函数方法
+val functionSum = { func: FuncType, value001: Int, value002: Int -> func(value001, value002) }
+
+val functionString: FuncType = { value001: Int, value002: Int -> "结果：${value001 + value002}" }
+
+
+// 内敛函数
+inline fun login(name: String, passWord: String, responseResult: (String, Int) -> Unit) {
+    if (name == "haoxuan" && passWord == "12345") {
+        responseResult("登录成功", 200)
+        return
+    }
+    responseResult("登录失败", 400)
+}
+
+fun methodResponseResult(msg: String, code: Int) {
+    println("登录结果：$msg code:$code")
+}
+
+// lambda类型函数就不需要加::
+val lambdaFunc = { msg: String, code: Int -> println("登录结果：$msg code:$code") }
