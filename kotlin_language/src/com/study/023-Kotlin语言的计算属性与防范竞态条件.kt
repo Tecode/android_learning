e19995167396b8df90b77b240a4149c60d66c620
Tecode <283731869@qq.com>
2022-04-21 20:38:58 +0800

    023-Kotlin语言的计算属性与防范竞态条件

--- kotlin_language/src/com/study/023-Kotlin语言的计算属性与防范竞态条件.kt
+++ kotlin_language/src/com/study/023-Kotlin语言的计算属性与防范竞态条件.kt
@@ -1,5 +1,17 @@
 package com.study
 
-fun main() {
+class ManagerClass {
+    var name: String? = "haoxuan"
+    fun getInfoName(): String {
+        return name?.also {
+            "最终的结果$it"
+        } ?: "name是null，请检查代码..."
+    }
+}
 
+fun main() {
+    val manager: ManagerClass = ManagerClass()
+    println(manager.getInfoName()) // haoxuan
+    manager.name = null
+    println(manager.getInfoName()) // name是null，请检查代码...
 }
