85da9dd4e1c66ab0f015511f9ecca33f1cb6e12d
Tecode <283731869@qq.com>
2022-05-12 00:10:02 +0800

    方法

--- helloAndroid/app/src/main/java/com/example/helloandroid/SwitchActivity.java
+++ helloAndroid/app/src/main/java/com/example/helloandroid/SwitchActivity.java
@@ -3,6 +3,7 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SwitchActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class SwitchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.switch_activity);
+        Switch switch_button = findViewById(R.id.switch_button);
+        switch_button.setOnCheckedChangeListener((group, checkedId) -> {
+
+        });
     }
 }
