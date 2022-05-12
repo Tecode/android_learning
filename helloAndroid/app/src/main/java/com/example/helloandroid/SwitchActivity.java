87cede67098489523ce9b35f27de0292eea8e6fe
Tecode <283731869@qq.com>
2022-05-12 23:36:21 +0800

    Switch、ToggleButton方法使用

--- helloAndroid/app/src/main/java/com/example/helloandroid/SwitchActivity.java
+++ helloAndroid/app/src/main/java/com/example/helloandroid/SwitchActivity.java
@@ -2,18 +2,47 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class SwitchActivity extends AppCompatActivity {
+public class SwitchActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch switch_button;
+    private ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.switch_activity);
-        Switch switch_button = findViewById(R.id.switch_button);
-        switch_button.setOnCheckedChangeListener((group, checkedId) -> {
+        switch_button =(Switch) findViewById(R.id.switch_button);
+        toggleButton =(ToggleButton) findViewById(R.id.toggle_button);
+        switch_button.setOnCheckedChangeListener(this);
+        toggleButton.setOnCheckedChangeListener(this);
+    }
 
-        });
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.switch_button:
+                if (compoundButton.isChecked()) {
+                    Toast.makeText(this, "Switch打开", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Switch关闭", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.toggle_button:
+                if (compoundButton.isChecked()) {
+                    Toast.makeText(this, "打开声音", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "关闭声音", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
