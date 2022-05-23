d9e15200e2b80b29f423ae1eef5a0710d6dd877d
Tecode <283731869@qq.com>
2022-05-23 22:29:12 +0800

    Snackbar的使用

--- helloAndroid/app/src/main/java/com/example/helloandroid/ToastActivity.java
+++ helloAndroid/app/src/main/java/com/example/helloandroid/ToastActivity.java
@@ -13,6 +13,10 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.time.Duration;
+
 public class ToastActivity extends AppCompatActivity {
 
     @Override
@@ -23,6 +27,8 @@ public class ToastActivity extends AppCompatActivity {
         final Toast toast = Toast.makeText(this, "Toast提示文本信息", Toast.LENGTH_SHORT);
         //设置Toast显示的位置
         toast.setGravity(Gravity.TOP, 0, 0);
+        final Snackbar snackbar = Snackbar.make(getWindow().getDecorView(),"Toast提示文本信息", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setText("哈哈，修改了文案");
         //自定义toast布局，不能自定义已经废除 This method was deprecated in API level 30
 //        LinearLayout layout = (LinearLayout) toast.getView();
 //        TextView v = layout.findViewById(android.R.id.message);
@@ -33,6 +39,6 @@ public class ToastActivity extends AppCompatActivity {
 //        layout.addView(image, 0);
 //
         Button btn = findViewById(R.id.btn);
-        btn.setOnClickListener(v1 -> toast.show());
+        btn.setOnClickListener(v1 -> snackbar.show());
     }
 }
