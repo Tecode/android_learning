ea5dd34d1eddc275d80c9594c679309cabb86b31
Tecode <283731869@qq.com>
2023-04-18 19:50:30 +0800

    provider使用和注册

--- helloAndroid/app/src/main/java/com/example/helloandroid/AlertDialogActivity.java
+++ helloAndroid/app/src/main/java/com/example/helloandroid/AlertDialogActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlertDialogActivity extends AppCompatActivity implements View.OnClickListener {
